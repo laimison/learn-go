b47655a11a226bfe4825098ecd0d5427407ca202
Laimison <laimison1@gmail.com>
2018-12-27 10:13:25 +0000

    conditional statements

---
+++ conditional_statements.go
@@ -0,0 +1,40 @@
+//usr/bin/env go run "$0" "$@"; exit
+
+package main
+
+import (
+  "fmt"
+)
+
+var _ = fmt.Printf
+
+func main() {
+  // if else
+  if num := 9; num < 0 {
+    fmt.Println(num, "is negative")
+  } else if num < 10 {
+    fmt.Println(num, "has 1 digit")
+  } else {
+    fmt.Println(num, "has multiple digits")
+  }
+
+  // switch case
+  i := 2
+  switch i {
+  case 1:
+    fmt.Println("one")
+  case 2:
+    fmt.Println("two")
+  default:
+    fmt.Println("none")
+  }
+
+  // loop
+  sum := 0
+  for i := 0; i < 10; i++ {
+    sum += i
+  }
+  fmt.Println(sum)
+
+
+}
